e8db4a50b04c18f9046a2315d47e8f486e08d0b1
Daruma <hectoryoramquiroz@gmail.com>
2024-08-09 17:22:11 -0600

    Adding openGL support

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./system/hardware-configuration.nix
       ./system/hyprland
+      ./system/openGL.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
