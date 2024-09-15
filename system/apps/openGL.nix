3cf24e263d0d180cb55f6ac8bcc316bfc1cc5480
Daruma <hectoryoramquiroz@gmail.com>
2024-09-15 14:05:08 -0600

    Adding docker

--- system/apps/openGL.nix
+++ system/apps/openGL.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
 
-  environment.systemPackages = with pkgs; [ glew gcc glfw libGL gdb mesa cmake libglvnd libGLU meson rpi-imager quartus-prime-lite unzip ];
+  environment.systemPackages = with pkgs; [ glew gcc glfw libGL gdb mesa cmake libglvnd libGLU meson rpi-imager quartus-prime-lite unzip cifs-utils ];
 
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
